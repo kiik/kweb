948076ebd6f7d53efd22bcc9b9403df4e4f55b33
Meelik Kiik <kiik.meelik@gmail.com>
2016-01-29 16:30:02 +0200

    Add JSON post example.

--- firmware/examples/simple-post.cpp
+++ firmware/examples/simple-post.cpp
@@ -1,5 +1,5 @@
-/** @file simple-request.cpp
- *  A simple HTTP GET request example.
+/** @file simple-post.cpp
+ *  A simple HTTP POST request example.
  *
  *  @author Meelik Kiik
  *  @date 29. January 2015
@@ -8,14 +8,18 @@
 
 #include "kiikweb.h"
 
-uint32_t request_time = 0;
-uint32_t request_period = 10 * 1000;
+uint32_t post_time = 0;
+uint32_t post_period = 10 * 1000;
 
-int request_data() {
+int32_t database[] = {1, 2, -3, 4, -2};
+
+int post_data() {
   HTTP::Transfer* trans = HTTP::getAvailableSocket();
   if(trans == nullptr) return -1;
 
-  HTTP::Request* req = new HTTP::Request("google.com", String::format("/"));
+  String* body = new String("{'data': [1, 2, -3, 4, -2]}");
+
+  HTTP::Request* req = new HTTP::Request("google.com", String::format("/"), body, HTTP::Method::POST, HTTP::ContentType::JSON);
 
   trans->setup(req);
   trans->setHandler([](HTTP::Request* req, HTTP::Response* res) {
@@ -30,14 +34,14 @@ int request_data() {
   });
 
   trans->start();
-  Serial.println("Sending request.");
+  Serial.println("Sending data.");
 
   return 0;
 }
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("simple-request.cpp");
+  Serial.println("simple-post.cpp");
 
   HTTP::setup();
 }
@@ -45,8 +49,8 @@ void setup() {
 void loop() {
   HTTP::process();
 
-  if(millis() > request_time) {
-    request_data();
-    request_time = millis() + request_period;
+  if(millis() > post_time) {
+    post_data();
+    post_time = millis() + post_period;
   }
 }
